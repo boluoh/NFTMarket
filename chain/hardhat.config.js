ef6cbefaff1340ce0b4c60ccf089277beb61e2fe
xuyh <x228832102@163.com>
2022-07-28 23:46:49 +0800

    add network ropsten

--- chain/hardhat.config.js
+++ chain/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const INFURA_PROJECT_ID = "YOUR INFURA PROJECT ID";
+
+const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
