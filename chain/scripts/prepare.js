29f3ab751e355bb6f7b377ea33f5ee4450a0abac
xuyh <x228832102@163.com>
2022-07-28 23:47:35 +0800

    update webapp

--- chain/scripts/prepare.js
+++ chain/scripts/prepare.js
@@ -10,24 +10,28 @@ async function main() {
 
   /* deploy the NFT contract */
   const NFT = await ethers.getContractFactory("BadgeToken")
-  const nftContract = await NFT.deploy(_name,_symbol)
+  const nftContract = await NFT.deploy(_name, _symbol)
   await nftContract.deployed()
+
+  const tokenAddress = nftContract.address;
 
   /* deploy the marketplace */
   const Market = await ethers.getContractFactory("NFTMarketplace")
   const marketContract = await Market.deploy()
 
-  console.log("nftContractAddress:",nftContract.address)
-  console.log("marketAddress     :",marketContract.address)
+  const marketAddress = marketContract.address;
+
+  console.log("nftContractAddress:", nftContract.address)
+  console.log("marketAddress     :", marketContract.address)
 
   console.log("========   Prepare for webapp dev ======")
-  console.log("nftContractAddress:",tokenAddress)
-  console.log("marketAddress     :",marketAddress)
+  console.log("nftContractAddress:", tokenAddress)
+  console.log("marketAddress     :", marketAddress)
   console.log("**should be the same**")
 
-  let owner,account1,account2
+  let owner, account1, account2
 
-  [owner, account1,account2] = await ethers.getSigners()
+  [owner, account1, account2] = await ethers.getSigners()
   const address0 = await owner.getAddress()
   const address1 = await account1.getAddress()
   const address2 = await account2.getAddress()
@@ -38,39 +42,39 @@ async function main() {
   const listingFee = await market.getListingFee()
   const auctionPrice = ethers.utils.parseUnits('1', 'ether')
 
-  console.log("1. == mint 1-6 to account#0")
-  for(let i=1;i<=6;i++){
+  console.log("1. == mint 1-6 to account#0,%", address0)
+  for (let i = 1; i <= 6; i++) {
     await nft.mintTo(address0)
   }
 
   console.log("2. == list 1-6 to market")
-  for(let i=1;i<=6;i++){
-    await nft.approve(marketAddress,i)
+  for (let i = 1; i <= 6; i++) {
+    await nft.approve(marketAddress, i)
     await market.createMarketItem(tokenAddress, i, auctionPrice, { value: listingFee })
   }
 
-  console.log("3. == mint 7-9 to account#1")
-  for(let i=7;i<=9;i++){
+  console.log("3. == mint 7-9 to account#1,%", address1)
+  for (let i = 7; i <= 9; i++) {
     await nft.connect(account1).mintTo(address1)
   }
 
   console.log("4. == list 1-6 to market")
-  for(let i=7;i<=9;i++){
-    await nft.connect(account1).approve(marketAddress,i)
+  for (let i = 7; i <= 9; i++) {
+    await nft.connect(account1).approve(marketAddress, i)
     await market.connect(account1).createMarketItem(tokenAddress, i, auctionPrice, { value: listingFee })
   }
 
-  console.log("5. == account#0 buy 7 & 8")
-  await market.createMarketSale(tokenAddress, 7, { value: auctionPrice})
-  await market.createMarketSale(tokenAddress, 8, { value: auctionPrice})
+  console.log("5. == account#0,% buy 7 & 8", address0)
+  await market.buyMarketItem(tokenAddress, 7, { value: auctionPrice })
+  await market.buyMarketItem(tokenAddress, 8, { value: auctionPrice })
 
-  console.log("6. == account#1 buy 1")
-  await market.connect(account1).createMarketSale(tokenAddress, 1, { value: auctionPrice})
+  console.log("6. == account#1,% buy 1", address1)
+  await market.connect(account1).buyMarketItem(tokenAddress, 1, { value: auctionPrice })
 
-  console.log("7. == account#2 buy 2")
-  await market.connect(account2).createMarketSale(tokenAddress, 2, { value: auctionPrice})
+  console.log("7. == account#2,% buy 2", address2)
+  await market.connect(account2).buyMarketItem(tokenAddress, 2, { value: auctionPrice })
 
-  console.log("8. == account#0 delete 6")
+  console.log("8. == account#0,% delete 6", address0)
   await market.deleteMarketItem(6)
 
 }
--- webapp/src/components/ReadNFTMarket.tsx
+++ webapp/src/components/ReadNFTMarket.tsx
@@ -1,61 +1,61 @@
 // components/ReadNFTMarket.tsx
 import React from 'react'
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useWeb3React } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { Contract } from "@ethersproject/contracts";
 import { Grid, GridItem, Box, Text, Button } from "@chakra-ui/react"
 import { BigNumber, ethers } from 'ethers';
 import useSWR from 'swr'
-import { addressNFTContract, addressMarketContract }  from '../projectsetting'
-import  CardERC721  from "./CardERC721"
+import { addressNFTContract, addressMarketContract } from '../projectsetting'
+import CardERC721 from "./CardERC721"
 
 interface Props {
-    option: number
+  option: number
 }
 
-export default function ReadNFTMarket(props:Props){
+export default function ReadNFTMarket(props: Props) {
   const abiJSON = require("abi/NFTMarketplace.json")
   const abi = abiJSON.abi
-  const [items,setItems] = useState<[]>()
+  const [items, setItems] = useState<[]>()
 
-  const {  account, active, library} = useWeb3React<Web3Provider>()
-  
+  const { account, active, library } = useWeb3React<Web3Provider>()
+
   // const { data: items} = useSWR([addressContract, 'fetchActiveItems'], {
   //   fetcher: fetcher(library, abi),
   // })
 
-useEffect( () => {
-    if(! active)
+  useEffect(() => {
+    if (!active)
       setItems(undefined)
 
-    if(!(active && account && library)) return
+    if (!(active && account && library)) return
 
     // console.log(addressContract,abi,library)
-    const market:Contract = new Contract(addressMarketContract, abi, library);
+    const market: Contract = new Contract(addressMarketContract, abi, library);
     console.log(market.provider)
     console.log(account)
 
-    library.getCode(addressMarketContract).then((result:string)=>{
+    library.getCode(addressMarketContract).then((result: string) => {
       //check whether it is a contract
-      if(result === '0x') return
+      if (result === '0x') return
 
-      switch(props.option){
+      switch (props.option) {
         case 0:
-          market.fetchActiveItems({from:account}).then((items:any)=>{
-            setItems(items)
-          })    
+          market.fetchActiveItems(1, 10, { from: account }).then((items: any) => {
+            setItems(items["pageItems"])
+          })
           break;
         case 1:
-          market.fetchMyPurchasedItems({from:account}).then((items:any)=>{
-            setItems(items)
-          })    
+          market.fetchMyPurchasedItems(1, 10, { from: account }).then((items: any) => {
+            setItems(items["pageItems"])
+          })
           break;
         case 2:
-          market.fetchMyCreatedItems({from:account}).then((items:any)=>{
-            setItems(items)
-            console.log(items)
-          })    
+          market.fetchMyCreatedItems(1, 10, { from: account }).then((items: any) => {
+            setItems(items["pageItems"])
+            console.log(items["pageItems"])
+          })
           break;
         default:
       }
@@ -63,53 +63,53 @@ useEffect( () => {
     })
 
     //called only when changed to active and account changed
-},[active,account])
+  }, [active, account])
 
 
-async function buyInNFTMarket(event:React.FormEvent,itemId:BigNumber) {
-  event.preventDefault()
+  async function buyInNFTMarket(event: React.FormEvent, itemId: BigNumber) {
+    event.preventDefault()
 
-  if(!(active && account && library)) return
+    if (!(active && account && library)) return
 
-  //TODO check whether item is available beforehand
+    //TODO check whether item is available beforehand
 
-  const market:Contract = new Contract(addressMarketContract, abi, library.getSigner());
-  const auctionPrice = ethers.utils.parseUnits('1', 'ether')
-  market.createMarketSale(
-      addressNFTContract, 
-      itemId, 
-      { value: auctionPrice}
+    const market: Contract = new Contract(addressMarketContract, abi, library.getSigner());
+    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
+    market.buyMarketItem(
+      addressNFTContract,
+      itemId,
+      { value: auctionPrice }
     ).catch('error', console.error)
-}
+  }
 
-const state = ["On Sale","Sold","Inactive"]
+  const state = ["On Sale", "Sold", "Inactive"]
 
-return (
-  <Grid templateColumns='repeat(3, 1fr)' gap={0} w='100%'>
+  return (
+    <Grid templateColumns='repeat(3, 1fr)' gap={0} w='100%'>
 
-    {items
-    ? 
-    (items.length ==0)
-      ?<Box>no item</Box>
-      :items.map((item:any)=>{
-        return(
-          <GridItem key={item.id} >
-            <CardERC721 addressContract={item.nftContract} tokenId={item.tokenId} ></CardERC721>
-            <Text fontSize='sm' px={5} pb={1}> {state[item.state]} </Text> 
-            {((item.seller == account && item.buyer == ethers.constants.AddressZero) || (item.buyer == account))
-            ?<Text fontSize='sm' px={5} pb={1}> owned by you </Text> 
-            :<Text></Text>
-            }
-            <Box>{
-            (item.seller != account && item.state == 0)
-            ? <Button width={220} type="submit" onClick={(e)=>buyInNFTMarket(e,item.id)}>Buy this!</Button>
-            : <Text></Text>
-            }
-            </Box>
-          </GridItem>)
-      })
-    :<Box></Box>}
-  </Grid>
+      {items
+        ?
+        (items.length == 0)
+          ? <Box>no item</Box>
+          : items.map((item: any) => {
+            return (
+              <GridItem key={item.id} >
+                <CardERC721 addressContract={item.nftContract} tokenId={item.tokenId} ></CardERC721>
+                <Text fontSize='sm' px={5} pb={1}> {state[item.state]} </Text>
+                {((item.seller == account && item.buyer == ethers.constants.AddressZero) || (item.buyer == account))
+                  ? <Text fontSize='sm' px={5} pb={1}> owned by you </Text>
+                  : <Text></Text>
+                }
+                <Box>{
+                  (item.seller != account && item.state == 0)
+                    ? <Button width={220} type="submit" onClick={(e) => buyInNFTMarket(e, item.id)}>Buy this!</Button>
+                    : <Text></Text>
+                }
+                </Box>
+              </GridItem>)
+          })
+        : <Box></Box>}
+    </Grid>
 
   )
 }
