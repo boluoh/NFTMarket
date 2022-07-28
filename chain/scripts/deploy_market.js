d5589c302c76a9d5f4768ab9a7094381af7f4013
xuyh <x228832102@163.com>
2022-07-28 23:47:21 +0800

    add deploy script

---
+++ chain/scripts/deploy_market.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+async function main() {
+
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log(
+        "Deploying contracts with the account:",
+        await deployer.getAddress()
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+
+    await token.deployed();
+
+    console.log("Token address:", token.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
